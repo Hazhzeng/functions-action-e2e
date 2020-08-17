4383778ceef82536555e5337130e0e3f7859a1a2
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-08-17 15:07:54 -0700

    PMPQA

--- tests/python/HttpTrigger/__init__.py
+++ tests/python/HttpTrigger/__init__.py
@@ -5,6 +5,6 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
-        "FAQQQ",
+        "PMPQA",
         status_code=200
     )
