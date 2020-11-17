a6d547c2c031b1ba8c398d30232218082a8f11df
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-11-17 11:50:24 -0800

    Commit

--- tests/python/HttpTrigger/__init__.py
+++ tests/python/HttpTrigger/__init__.py
@@ -5,6 +5,6 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
-        "KKKKK",
+        "QQQQQ",
         status_code=200
     )
