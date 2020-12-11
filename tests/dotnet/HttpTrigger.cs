d4a75d88955aa8d3b0eac4a974799fe35783aca6
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-12-10 22:25:54 -0800

    Test KKKKK

--- tests/dotnet/HttpTrigger.cs
+++ tests/dotnet/HttpTrigger.cs
@@ -17,7 +17,7 @@ namespace dotnet
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            return (ActionResult)new OkObjectResult("JSKDLA");
+            return (ActionResult)new OkObjectResult("KKKKK");
         }
     }
 }
