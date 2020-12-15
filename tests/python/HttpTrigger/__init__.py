738c65b1e0f524929e4307ba66b09dfd82a5bb35
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-12-15 13:08:22 -0800

    Check funcignore

--- tests/python/HttpTrigger/__init__.py
+++ tests/python/HttpTrigger/__init__.py
@@ -5,6 +5,6 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
-        "QQQQQ",
+        "JKLASDF",
         status_code=200
     )
