6424f9e0d2d722524e5ac4c2976f8e76060874be
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2021-01-28 15:52:42 -0800

    Start

--- tests/node/HttpTrigger/index.ts
+++ tests/node/HttpTrigger/index.ts
@@ -2,7 +2,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    context.res = { body: "WHATWHATWHAT" };
+    context.res = { body: "GOGOGO" };
 };
 
 export default httpTrigger;
--- tests/python/HttpTrigger/__init__.py
+++ tests/python/HttpTrigger/__init__.py
@@ -6,6 +6,6 @@ import pyodbc
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
-        "WHATWHATWHAT",
+        "GOGOGO",
         status_code=200
     )
